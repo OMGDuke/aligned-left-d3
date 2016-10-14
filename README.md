f212cf25afe956b089cc9b3a826b9f1318014eb2
OMGDuke <lex1989@gmail.com>
2016-10-14 19:22:30 +0100

    Refactored to pull out functions from d3 style assignments

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # aligned-left-d3
-Learning d3.js
+Learning d3.js using [this](http://alignedleft.com/tutorials/d3/) tutorial from Scott Murray.
--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-var dataset = [ 5, 10, 15, 20, 25 ];
+var dataset = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19, 14, 11, 22, 29, 11, 13, 12,
+                17, 18, 10, 24, 18, 25, 9, 3 ];
+
+
+d3.select("body")
+  .selectAll("div")
+  .data(dataset)
+  .enter()
+  .append("div")
+  .attr("class", "bar")
+  .style("height", setBarHeights);
 
 d3.select("body")
   .selectAll("p")
@@ -8,21 +18,17 @@ d3.select("body")
   .text(function(d, i) {
     return "Array Element "+ (i + 1) + ": " + d;
   })
-  .style("color", function(d) {
-    if(d > 15) {
-      return "red";
-    } else {
-      return "black";
-    }
-  });
+  .style("color", setTextColor);
 
-d3.select("body")
-  .selectAll("div")
-  .data(dataset)
-  .enter()
-  .append("div")
-  .attr("class", "bar")
-  .style("height", function(d) {
+function setBarHeights(d) {
     var barHeight = d * 5;
     return barHeight + "px";
-  });
+}
+
+function setTextColor(d) {
+  if(d > 15) {
+    return "red";
+  } else {
+    return "black";
+  }
+}
