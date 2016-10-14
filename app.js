341c23083241ee1385076a075e92b72b139e1ba7
OMGDuke <lex1989@gmail.com>
2016-10-14 19:12:33 +0100

    Drawing a basic chart with divs

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ d3.select("body")
   .data(dataset)
   .enter()
   .append("p")
-  .text(function(d) {
-    return "I can count up to " + d;
+  .text(function(d, i) {
+    return "Array Element "+ (i + 1) + ": " + d;
   })
   .style("color", function(d) {
     if(d > 15) {
@@ -14,4 +14,15 @@ d3.select("body")
     } else {
       return "black";
     }
+  });
+
+d3.select("body")
+  .selectAll("div")
+  .data(dataset)
+  .enter()
+  .append("div")
+  .attr("class", "bar")
+  .style("height", function(d) {
+    var barHeight = d * 5;
+    return barHeight + "px";
   });
